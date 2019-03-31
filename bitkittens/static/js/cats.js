03a7b509c6582f4aaf9ee35ef110be400dda7a5b
Anton <anton.mois33v@gmail.com>
2019-03-31 09:30:45 -0400

    Assignment complete + 1 stretch goal

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const summonKittens = document.querySelector('.summon-cats');
+  summonKittens.addEventListener('click', function(event) {
+    const catDivs = document.querySelectorAll('.cat-box');
+    // convert our nodelist to an array
+    const catArray = Array.from(catDivs);
+    axios.get('http://bitkittens.herokuapp.com/cats.json', {'params': {'number': 5}}).then(function(response) {
+      response.data.cats.forEach((cat) => {
+        const newImg = document.createElement('img');
+        newImg.src = cat.photo;
+        newImg.alt = `Photo of ${cat.name}`;
+        const randomCatDiv = catArray[Math.floor(Math.random()*catArray.length)];
+        catArray.splice(catArray.indexOf(randomCatDiv), 1);
+        console.log(randomCatDiv);
+        // clear HTML contents of div before appending the new img
+        randomCatDiv.innerHTML = "";
+        randomCatDiv.append(newImg);
+      });
+    })
+  });
 });
